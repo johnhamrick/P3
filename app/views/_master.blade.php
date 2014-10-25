7b6f950c561b3a419cc2800f8bde0d1958bb539f
John Hamrick <john@hamrickjohn.me>
2014-10-24 23:58:45 -0400

    Code edits

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-@include('header')
-</head>
-<body>
-@include('nav')
-@include('jumbotron')
-@yield('body')	
-@include('footer')
-</body>
+		<head>
+			@include('header')
+		</head>
+		<body>
+			@include('nav')
+			@include('jumbotron')
+			@yield('body')	
+			@include('footer')
+		</body>
 </html>
