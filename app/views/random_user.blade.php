cd543c0a10f8f8e54a02f9b5daf678a06f39ccdc
John Hamrick <john@hamrickjohn.me>
2014-10-28 23:52:32 -0400

    Code updates

--- app/views/random_user.blade.php
+++ app/views/random_user.blade.php
@@ -18,7 +18,7 @@
 		<div class="col-md-4">
 			<h3>About</h3>
 		<p>
-		Random Uers are often needed for testing in software applications. This Random User Generator provides fictitious user(s) with the following parameters:
+		Random Users are often needed for testing in software applications. This Random User Generator provides fictitious user(s) with the following parameters:
 		</p>
 			<ul>
 				<li>Name</li>
